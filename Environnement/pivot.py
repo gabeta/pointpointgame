bbdbf938d0beadbeb134f48a8d6061902f0c6f2f
Gabeta Soro <gabeta.soro@gmail.com>
2017-06-26 16:53:17 +0000

    Création des functions permettants de savoir si il y a une possibilité de marquer un carré

--- Environnement/pivot.py
+++ Environnement/pivot.py
@@ -67,3 +67,57 @@ class Pivot(object):
       if(P2 in point_dico) & (P3 in point_dico) & (P4 in point_dico):
           if(point_dico[P] == point_dico[P2]) & (point_dico[P] == point_dico[P3]) & (point_dico[P] == point_dico[P4]):
               return True
+
+
+  def get_point_top(self,x,y,space,point_dico):
+      x2 = x - space
+      y2 = y - space
+      P = str(str(x)+'_'+str(y))
+      P2 = str(str(x)+'_'+str(y2))
+      P3 = str(str(x2)+'_'+str(y))
+      P4 = str(str(x2)+'_'+str(y2))
+
+      if(P2 in point_dico):
+          return False
+      else:
+          if(P in point_dico) & (P3 in point_dico) & (P4 in point_dico):
+              if(point_dico[P] == point_dico[P3]) & (point_dico[P] == point_dico[P4]):
+                  return {x,y2}
+
+
+  def get_point_right(self,x,y,space,point_dico):
+      x2 = x - space
+      y2 = y - space
+      P = str(str(x)+'_'+str(y))
+      P2 = str(str(x)+'_'+str(y2))
+      P3 = str(str(x2)+'_'+str(y))
+      P4 = str(str(x2)+'_'+str(y2))
+
+      if(P2 in point_dico):
+          return False
+      else:
+          if(P in point_dico) & (P3 in point_dico) & (P4 in point_dico):
+              if(point_dico[P] == point_dico[P3]) & (point_dico[P] == point_dico[P4]):
+                  return {x,y2}
+
+
+  def get_point_top_left(self):
+      pass
+
+  def get_point_top_rigth(self):
+      pass
+
+  def get_point_bottom(self):
+      pass
+
+  def get_point_left(self):
+      pass
+
+  def get_point_bottom_left(self):
+      pass
+
+  def get_point_bottom_right(self):
+      pass
+
+
+
--- IA/IrisIA.py
+++ IA/IrisIA.py
@@ -10,6 +10,8 @@
 #
 ########################################################
 
+from Environnement.pivot import Pivot
+from Environnement.terrain import Terrain
 
 class IrisIA(object):
 
@@ -18,6 +20,26 @@ class IrisIA(object):
         self.otherDico = {}
         self.color = color
         self.score = 0
+
+    def checkIsFavorablePosition(self,dico):
+        #Parcourt le dictionnaire
+        for k in dico:
+            #Exploder le K par les enderscorts
+            array = k.split('_')
+            x = int(array[0])
+            y = int(array[1])
+            top = Pivot.get_point_top(x,y,Terrain.getSpace(),dico)
+
+            #Vérifie si il y a une possibilité de marquer
+            pass
+
+    def checkOwnDico(self):
+        if self.checkIsFavorablePosition():
+            pass
+
+    def checkOtherDico(self):
+        if self.checkIsFavorablePosition():
+            pass
 
     #Dictionnaire de point de l'IA. ce Dictionnaire permettra de connaitre tous les emplacements des points de l'IA.
     def getOwnDico(self):
