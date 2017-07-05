d3af34ff08cadf80ebdb011231ccbc1b627c9f3b
Gabeta Soro <gabeta.soro@gmail.com>
2017-07-05 16:36:49 +0000

    Début gestion de l'IAirisCE

--- IA/irisCeIA.py
+++ IA/irisCeIA.py
@@ -4,10 +4,10 @@
 # Catégorie        : Stratégie
 # Auteur           : Gabeta Soro (Alchimiste des codes)
 # Compagnie        : EnighmaLab
-# Date de création : 26/06/2017
+# Date de création : 05/07/2017
 # Module           : Intelligence Articielle
-# Numéro IA        : Iris-CP12
-# Niveau           : CP1/CP2
+# Numéro IA        : Iris-CE12
+# Niveau           : CE1/CE2
 #
 ########################################################
 
@@ -22,16 +22,24 @@ class IrisCpIA(IrisIA):
 
         defense_dico = self.defenseDico
 
-        #Renvoie du premier point d'une manière aléatoire
-        for k in defense_dico:
-            array = k.split('_')
-            x = int(array[0])
-            y = int(array[1])
-            final_dico = {}
-            final_dico['x'] = x
-            final_dico['y'] = y
+        #Ranger le ditionnaire dans l'ordre decroissant en fonction de clés
 
-            return final_dico
+        #Récupère le premier élement du dictionnaire
+
+        #Création du dictionnaire final
+        k = '30_30'
+        array = k.split('_')
+        x = int(array[0])
+        y = int(array[1])
+        final_dico = {}
+        final_dico['x'] = x
+        final_dico['y'] = y
+
+        #Vidage du dictionnaire de defense
+
+        #retour du dictionnaire final
+        return final_dico
+
 
     def attack(self):
         attack_dico = self.attackDico
